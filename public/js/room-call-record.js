1b6f43f17737a9e10b2cf9a2a79d9ea4899a7cb5
huy.ntq <huy.ntq02@gmail.com>
2023-12-02 11:51:27 +0700

    Update room-call.css and room-call-record.js

--- public/js/room-call-record.js
+++ public/js/room-call-record.js
@@ -1,119 +1,69 @@
 /* eslint-disable no-undef */
-// let isRecording = false;
-let isRecording = true;
+let isRecording = false;
+let timerInterval;
+let recordingDuration = 0;
+
 $(() => {
-    // Assuming you have a WebRTC peer connection named 'peerConnection' and video elements with IDs 'localVideo' and 'remoteVideo'
     let localVideo = document.getElementById('localVideo');
     let remoteVideo = document.getElementById('remoteVideo');
-    const codecPreferences = document.querySelector('#codecPreferences');
 
     let mediaRecorder;
-    let recordedBlobs;
+    let recordedChunks = [];
 
-    function handleDataAvailable(event) {
-        console.log('handleDataAvailable', event);
-        if (event.data && event.data.size > 0) {
-            recordedBlobs.push(event.data);
+    function handleStream(stream) {
+        try {
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.ondataavailable = function (event) {
+                if (event.data.size > 0) {
+                    recordedChunks.push(event.data);
+                }
+            };
+
+            mediaRecorder.onstop = function () {
+                clearInterval(timerInterval);
+                let recordedBlob = new Blob(recordedChunks, { type: 'video/webm' });
+
+                let downloadLink = document.createElement('a');
+                downloadLink.href = URL.createObjectURL(recordedBlob);
+                downloadLink.download = 'recorded-video.webm';
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeChild(downloadLink);
+                recordedChunks = [];
+                recordingDuration = 0;
+                $('#timer').text('');
+            };
+        } catch (error) {
+            console.log(`handleStream 🚀 error`, error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'No video stream available. Please check your camera settings.',
+            });
         }
     }
 
     $('.record').on('click', async () => {
         let localStream = localVideo.srcObject;
-        let remoteStream = remoteVideo.srcObject;
 
         if (isRecording) {
-            console.log(`$ 🚀 localStream`, localStream);
-            recordedBlobs = [];
-            const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
-            const options = { mimeType };
-
-            try {
-                mediaRecorder = new MediaRecorder(window.stream, options);
-            } catch (e) {
-                console.error('Exception while creating MediaRecorder:', e);
-                return;
-            }
-
-            console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-            recordButton.textContent = 'Stop Recording';
-            mediaRecorder.onstop = (event) => {
-                console.log('Recorder stopped: ', event);
-                console.log('Recorded Blobs: ', recordedBlobs);
-            };
-            mediaRecorder.ondataavailable = handleDataAvailable;
-            mediaRecorder.start();
-            console.log('MediaRecorder started', mediaRecorder);
+            mediaRecorder.stop();
+            clearInterval(timerInterval);
 
             isRecording = false;
         } else {
-            stopRecording();
+            console.log(`$ 🚀 localStream`, localStream);
+            handleStream(localStream);
+
+            recordedChunks = [];
+            mediaRecorder.start();
+
+            timerInterval = setInterval(() => {
+                $('#timer').text(`Recording: ${moment.utc((++recordingDuration) * 1000).format('mm:ss')}`);
+            }, 1000);
+
             isRecording = true;
         }
-
-        // if (isRecording) {
-        //     // Stop recording
-        //     // mediaRecorder.stop();
-        //     isRecording = false;
-        //     console.log('Recording stopped');
-
-        //     console.log(`$ 🚀 localStream`, localStream);
-        //     let recorder = new RecordRTCPromisesHandler(localStream, {
-        //         type: 'video'
-        //     });
-        //     recorder.startRecording();
-        //     console.log('Recording stopped');
-
-        //     const sleep = m => new Promise(r => setTimeout(r, m));
-        //     console.log('Recording stopped');
-        //     await sleep(3000);
-        //     console.log('Recording stopped');
-
-        //     console.log('Recording stopped');
-        //     await recorder.stopRecording();
-        //     console.log('Recording stopped');
-        //     let blob = await recorder.getBlob();
-        //     console.log('Recording stopped');
-        //     invokeSaveAsDialog(blob);
-        //     console.log('Recording stopped');
-        // } else {
-        //     // Start recording
-        //     // mediaRecorder.start();
-        //     isRecording = true;
-        //     console.log('Recording started');
-        // }
     });
-
-    function stopRecording() {
-        mediaRecorder.stop();
-    }
-    // // Create MultiStreamRecorder instance
-    // const multiStreamRecorder = new MultiStreamRecorder([localStream, remoteStream], {
-    //     mimeType: 'video/webm',
-    //     audioContext: true,
-    //     previewStream: function (stream) {
-    //         // You can do something with the preview stream if needed
-    //     },
-    // });
-
-    // // Start recording when the button is clicked
-    // document.getElementById('startRecordingButton').addEventListener('click', () => {
-    //     multiStreamRecorder.start();
-    // });
-
-    // // Stop recording when the button is clicked
-    // document.getElementById('stopRecordingButton').addEventListener('click', () => {
-    //     multiStreamRecorder.stop();
-    // });
-
-    // // Handle the recorded data
-    // multiStreamRecorder.ondataavailable = function (blobs) {
-    //     // blobs is an array of Blob objects, one for each stream
-    //     const combinedBlob = new Blob(blobs, { type: 'video/webm' });
-
-    //     // Create a download link for the combined blob
-    //     const downloadLink = document.createElement('a');
-    //     downloadLink.href = URL.createObjectURL(combinedBlob);
-    //     downloadLink.download = 'combined-video.webm';
-    //     downloadLink.click();
-    // };
 });
